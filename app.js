4989386eb4438777601041aa5a69ab3b6139da26
Jonathan <Jonathaninabraga@gmail.com>
2025-05-24 20:02:26 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+angular.module('blog', [])
+.controller('Rest', function ($scope, $http){
+  $http.get('http://localhost:8080/postagens/')
+    .then(function(response) {
+      $scope.publicacoes = response.data;
+      $scope.index = function(index) {
+        localStorage.setItem("index", index);
+      };
+    }, function(error) {
+      console.error("Erro ao carregar postagens:", error);
+    });
+});
+
+angular.module('blogMax', [])
+.controller('RestMax', function ($scope, $http){
+  $http.get('http://localhost:8080/postagens/')
+    .then(function(response) {
+      const index = localStorage.getItem("index");
+      $scope.publicacoes = response.data[index];
+    }, function(error) {
+      console.error("Erro ao carregar postagem:", error);
+    });
+});
